f01802b1cae57cd0969c4564fa5193560451aa5f
felipecaninnovaes <felipecaninn18@gmail.com>
2020-01-08 20:06:30 -0300

    gnome tilix add

--- Gnome.sh
+++ Gnome.sh
@@ -2,7 +2,7 @@ systemctl enable NetworkManager
 pacman -S haveged
 systemctl enable haveged
 pacman --needed -S zsh
-sudo pacman --needed -S haveged networkmanager gnome-shell gdm gnome-photos gnome-disk-utility gnome-system-monitor mousetweaks gnome-font-viewer gedit gnome-menus gnome-photos gnome-dictionary gnome-logs nautilus gnome-control-center gnome-tweaks gnome-backgrounds adwaita-icon-theme zsh vlc git go ttf-dejavu ttf-freefont
+sudo pacman --needed -S haveged networkmanager tilix gnome-shell gdm gnome-photos gnome-disk-utility gnome-system-monitor mousetweaks gnome-font-viewer gedit gnome-menus gnome-photos gnome-dictionary gnome-logs nautilus gnome-control-center gnome-tweaks gnome-backgrounds adwaita-icon-theme zsh vlc git go ttf-dejavu ttf-freefont
 sudo systemctl enable gdm
 sudo systemctl enable NetworkManager
 sudo systemctl enable haveged
