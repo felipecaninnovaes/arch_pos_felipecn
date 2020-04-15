ee7ce813e917d11298006d903360cf41197ece54
FelipeCN <57776624+felipecaninnovaes@users.noreply.github.com>
2020-04-14 23:53:06 -0300

    Vulkan drivers

--- AmdGpu.sh
+++ AmdGpu.sh
@@ -1,4 +1,4 @@
 sudo echo "[multilib]" >>/etc/pacman.conf
 sudo echo "Include = /etc/pacman.d/mirrorlist" >>/etc/pacman.conf
-sudo pacman -Sy xf86-video-amdgpu lib32-mesa xorg-server amdvlk mesa lib32-mesa-libgl mesa-libgl xorg xorg-server linux-firmware lib32-libva-mesa-driver mesa-vdpau libva-mesa-driver --noconfirm
+sudo pacman -Sy xf86-video-amdgpu lib32-mesa xorg-server vulkan-radeon lib32-vulkan-radeon mesa lib32-mesa-libgl mesa-libgl xorg xorg-server linux-firmware lib32-libva-mesa-driver mesa-vdpau libva-mesa-driver vulkan-icd-loader lib32-vulkan-icd-loader--noconfirm
 
