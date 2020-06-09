5e1796135babde1b4acdb7044da67791ec730a99
FelipeCN <57776624+felipecaninnovaes@users.noreply.github.com>
2020-06-09 13:15:39 -0300

    add os-prober install

--- efi.sh
+++ efi.sh
@@ -1,2 +1,2 @@
-sudo pacman --needed -S efibootmgr grub --noconfirm
+sudo pacman --needed -S efibootmgr grub  os-prober --noconfirm
 grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=arch_grub_fcn && mkinitcpio -p linux && grub-mkconfig -o /boot/grub/grub.cfg && mkinitcpio -p linux
