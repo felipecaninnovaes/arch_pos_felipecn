8328699b2a3442afa077b10e54ef368a20fc9372
FelipeCN <57776624+felipecaninnovaes@users.noreply.github.com>
2020-06-09 13:22:50 -0300

    Remove unnecessary apps, add GDM

--- Gnome.sh
+++ Gnome.sh
@@ -1,8 +1,5 @@
 systemctl enable NetworkManager;
-pacman -S haveged;
-systemctl enable haveged;
 pacman --needed -S zsh;
-sudo pacman --needed -S haveged networkmanager tilix gnome-shell eog gnome-disk-utility gnome-system-monitor mousetweaks gnome-font-viewer gedit gnome-menus gnome-photos gnome-dictionary gnome-logs nautilus gnome-control-center gnome-tweaks gnome-backgrounds adwaita-icon-theme zsh vlc git go ttf-dejavu ttf-freefont --noconfirm;
+sudo pacman --needed -S gdm networkmanager tilix gnome-shell gnome-disk-utility gnome-system-monitor mousetweaks gnome-font-viewer gedit gnome-menus gnome-photos gnome-dictionary gnome-logs nautilus gnome-control-center gnome-tweaks gnome-backgrounds adwaita-icon-theme zsh git go ttf-dejavu ttf-freefont --noconfirm;
 sudo systemctl enable gdm;
 sudo systemctl enable NetworkManager;
-sudo systemctl enable haveged;
