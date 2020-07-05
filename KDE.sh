80e2f4a921b037e2ef51f8be054891076c6588ab
FelipeCN <57776624+felipecaninnovaes@users.noreply.github.com>
2020-07-05 13:20:06 -0300

    add and remove packages

--- KDE.sh
+++ KDE.sh
@@ -1,6 +1,4 @@
 systemctl enable NetworkManager;
-pacman -S --needed haveged --noconfirm;
-systemctl enable haveged;
 pacman --needed -S zsh;
-sudo pacman --neede -S plasma konsole sshfs kdeconnect htop openssh vlc ksysguard gwenview spectacle plank kvantum-qt5 --noconfirm;
+sudo pacman --neede -S plasma konsole sshfs kdeconnect htop openssh vlc ksysguard gwenview spectacle kvantum-qt5 dolphin ark unzip unrar --noconfirm;
 sudo systemctl enable sddm;
