e6bec8e9b7c4c1afbff3b9ccadbabdc3b291ee43
FelipeCN <felipecaninn18@gmail.com>
2021-06-29 19:13:29 -0300

    add pipewire

--- source/Base.sh
+++ source/Base.sh
@@ -1,2 +1,2 @@
-sudo pacman --needed -S grub os-prober nano git go networkmanager zsh --noconfirm;
+sudo pacman --needed -S grub os-prober nano git go networkmanager zsh pipewire pipewire-pulse --noconfirm;
 sudo systemctl enable NetworkManager;
