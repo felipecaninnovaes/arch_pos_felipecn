c392cc36b16ed8f40d4b8661cf99b9078b7d938e
FelipeCN <57776624+felipecaninnovaes@users.noreply.github.com>
2021-07-11 09:15:41 -0300

    Update Base.sh

--- source/Base.sh
+++ source/Base.sh
@@ -1,2 +1,2 @@
-sudo pacman --needed -S grub os-prober nano git go wget curl networkmanager zsh pipewire pipewire-pulse --noconfirm;
+sudo pacman --needed -S grub os-prober nano git go wget curl networkmanager firefox zsh pipewire pipewire-pulse --noconfirm;
 sudo systemctl enable NetworkManager;
