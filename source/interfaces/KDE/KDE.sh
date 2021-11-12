44916cc6728f5b3aeda4720905fb5a3f39ff4652
FelipeCN <57776624+felipecaninnovaes@users.noreply.github.com>
2021-11-12 12:03:37 -0300

    Update KDE.sh

--- source/interfaces/KDE/KDE.sh
+++ source/interfaces/KDE/KDE.sh
@@ -1,4 +1,4 @@
 systemctl enable NetworkManager;
 pacman --needed -S zsh;
-sudo pacman --neede -S plasma konsole sshfs kdeconnect htop openssh vlc ksysguard gwenview spectacle kvantum-qt5 dolphin ark unzip unrar --noconfirm;
+sudo pacman --neede -S plasma konsole sshfs spectacle kdeconnect htop openssh vlc ksysguard gwenview spectacle kvantum-qt5 dolphin ark unzip unrar --noconfirm;
 sudo systemctl enable sddm;
